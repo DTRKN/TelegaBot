129a7450fd943d1ecd6bd4d90609ee7f3616b855
DTRKN <taratorkin.den@mail.ru>
2022-11-04 03:09:51 +0500

    replace Hello

---
+++ TelegaBot.py
@@ -0,0 +1,9 @@
+import telebot
+
+bot = telebot.TeleBot('5490555803:AAHaSCIWAKI2daODJj43wGxZhteSZc5oZIU')
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, 'Hello')
+
+bot.polling(none_stop=True)
