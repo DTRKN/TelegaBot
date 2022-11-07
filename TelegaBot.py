36d7612a336dd2ea0f9c61c49f19954c7f8601be
DTRKN <taratorkin.den@mail.ru>
2022-11-07 17:48:41 +0500

    Update

--- TelegaBot.py
+++ TelegaBot.py
@@ -4,6 +4,7 @@ bot = telebot.TeleBot('5490555803:AAHaSCIWAKI2daODJj43wGxZhteSZc5oZIU')
 
 @bot.message_handler(commands=['start'])
 def start(message):
+    mess = f'Привет, <b{message.from_user.first_name} <u>{message.from_user.last_name}</u></b>'
     bot.send_message(message.chat.id, 'Здарова')
 
 bot.polling(none_stop=True)
